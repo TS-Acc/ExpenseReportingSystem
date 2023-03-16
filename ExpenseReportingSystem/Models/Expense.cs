bc4ecf29989dc27fabcd1e7607fb6118a91ab7ee
Trevor Shepherd <t.rayshpd@gmail.com>
2023-03-16 13:19:15 -0400

    Fixed the Expense table!

--- ExpenseReportingSystem/Models/Expense.cs
+++ ExpenseReportingSystem/Models/Expense.cs
@@ -2,5 +2,16 @@
 {
     public class Expense
     {
+        // main table columns
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public decimal Total { get; set; }
+        public int EmployeeId { get; set; }
+
+        // virtual employee to hold the FK instance when reading an expense and a collection of expenselines related
+        // to this expense
+        public virtual Employee? Employees { get; set; }
+        public virtual ICollection<Expenseline> Expenselines { get; set; }
     }
 }
