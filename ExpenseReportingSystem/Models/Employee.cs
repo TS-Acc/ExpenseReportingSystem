aa26521bb65b8b422f8df5d985ff85339485d3d6
Tim Doren <dorentim@gmail.com>
2023-03-16 14:06:09 -0400

    Added string.Empty to employee string properties

--- ExpenseReportingSystem/Models/Employee.cs
+++ ExpenseReportingSystem/Models/Employee.cs
@@ -10,11 +10,11 @@ namespace ExpenseReportingSystem.Models
     {
         public int Id { get; set; }
         [StringLength(30)]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         [StringLength(50)]
-        public string Email { get; set; }
-        [StringLength (30)]
-        public string Password { get; set; }
+        public string Email { get; set; } = string.Empty;
+        [StringLength(30)]
+        public string Password { get; set; } = string.Empty;
         public bool Admin { get; set; }
         [Column(TypeName = "decimal(11,2)")]
         public decimal ExpensesDue { get; set; }
